7b7cd53430a46bd5e0341007a619b7475c68606a
Nathan M <nathanminissale@gmail.com>
2018-09-05 05:29:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,5 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
-    if (i = 1) {
-      array.push("I am 1 strange loops.")
-    } else {
-      array.push("I am ${i} strange loops.")
-    }
+    array.push("I am ${i} strange loops.")
   }
 }
